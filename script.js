47c395c43602e005f165a352f80b2261163b55cd
Glitch (cssi07-14) <none>
2021-07-15 16:10:30 +0000

    07/14

--- script.js
+++ script.js
@@ -1,37 +1,77 @@
-/* globals createCanvas background noFill strokeWeight stroke ellipse rect fill*/
+/* globals createCanvas colorMode HSB noStroke color background mouseX mouseY width height fill ellipse text stroke line noStroke */
 
-// Content behind double slashes is a comment. Use it for plain English notes,
-// or for code that you want to temporarily disable.
+// We'll use variables for most of our colors in this code-along.
+let backgroundColor, color1, color2, textColor, width, length, globalB, globalS;
 
 function setup() {
-  // Code here runs only once
-  createCanvas(800, 600);
+  // Canvas & color settings
+  width = 400;
+  length = 400;
+  createCanvas(width, length);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+
+  // When used with only one argument, the color mode is greyscale.
+  // 0 is black and 100 is white.
+  backgroundColor = color(95);
+  textColor = color(0);
+  // When used with three arguments, the function takes, in this order:
+  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
+  //       is blue.
+  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
+  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
+  globalS = 100;
+  globalB = 50;
+  color1 = color(0, globalS, globalB);
+  color2 = color(200, globalS, globalB);
+  
+  
 }
 
-function drawRings() {
-  // Code here runs continuously
-  noFill();
-  background(255);
-  strokeWeight(2);
-  //Ring 1: Blue
-  stroke(0, 133, 199);
-  ellipse(100, 100, 50);
-
-  //Ring 2: Yellow
-  stroke(244, 195, 0);
-  ellipse(130, 130, 50);
-
-  //Ring 3: Black
-  stroke(0, 0, 0);
-  ellipse(160, 100, 50);
-
-  //Ring 4: Green
-  stroke(0, 159, 61);
-  ellipse(190, 130, 50);
-
-  //Ring 5: Red
-  stroke(223, 0, 36);
-  ellipse(220, 100, 50);
-
+function draw() {
+  background(backgroundColor);
   
+  // Call the drawCenterLine function here to run the three lines of code
+  // contained in that function.
+  drawCenterLine();
+  if (mouseX < width/2) {
+    dayMode();
+  } 
+  else {
+    nightMode();
+  }
+
+  // The red and blue circles:
+  fill(color1);
+  ellipse(width * 0.25, length/2.0, 50);
+  fill(color2);
+  ellipse(width * 0.75, length/2.0, 50);
+
+  // The grey circle and the text:
+  fill(textColor);
+  ellipse(mouseX, mouseY, 50);
+  text("Flip the switch: (" + mouseX + ", " + mouseY + ")", 20, 20);
+}
+
+function drawCenterLine() {
+  // This function will turn stroke on, draw the line, and then turn stroke
+  // back off.
+  // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
+  stroke(textColor);
+  line(width/2.0, 0, width/2.0, length);
+  noStroke();
+}
+
+function nightMode() {
+  backgroundColor = color(0);
+  textColor = color(95);
+  color1 = color(200, globalS, globalB);
+  color2 = color(0, globalS, globalB);
+}
+
+function dayMode() {
+  backgroundColor = color(95);
+  textColor = color(0);
+  color1 = color(0, globalS, globalB);
+  color2 = color(200, globalS, globalB);
 }
